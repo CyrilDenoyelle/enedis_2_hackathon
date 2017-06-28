8ff18fe2c762af5a84fa98eefed6e724fe4cfa58
salwa <salwaoudad@gmail.com>
2017-06-28 15:02:09 +0200

    modifs style

--- resources/views/gauge.blade.php
+++ resources/views/gauge.blade.php
@@ -2,22 +2,26 @@
 
 @section('content')
 <link rel='stylesheet' href='/css/gaugestyle.css'>
-<main>
-<h2>consommation actuelle</h2>
-<div id='gaugecontainer'>
-	<img src='/img/compteur.png' alt='compteur' id='compteur'>
-	<img src='/img/aiguille.png' alt='aiguille' id='aiguille' style="transition: 2s;">
-	<span id='compteurspan' style=''>{{ $spaneuro }}€/h</span>
-</div>
-</main>
-<a class="btn btn-primary btn-block" href='/conso'>conso</a>
 
-@endsection
+	<div class="container">
+		<div class="row">
+			<h2>consommation actuelle</h2>
 
-@section('scriptation')
-<script>
-	setTimeout(function(){
-		$('#aiguille').css('transform', 'rotate({{ $consommation }}deg')
-	}, 2000)
-</script>
+			<div id='gaugecontainer' class="col-md-12">
+				<img src='/img/compteur.png' alt='compteur' id='compteur'>
+				<img src='/img/aiguille.png' alt='aiguille' id='aiguille' style="transition: 2s;">
+				<span id='compteurspan' style=''>{{ $spaneuro }}€/h</span>
+			</div>
+			<a class="btn btn-primary btn-block" href='/conso'>conso</a>
+
+			@endsection
+
+			@section('scriptation')
+			<script>
+				setTimeout(function(){
+					$('#aiguille').css('transform', 'rotate({{ $consommation }}deg')
+				}, 2000)
+			</script>
+		</div>
+	</div>
 @endsection
