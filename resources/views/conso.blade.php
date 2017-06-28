baf6b730dc8c772de6f2bfa819951e489ca351fb
lapie002 <lapierre.bruno@gmail.com>
2017-06-28 10:33:19 +0200

    conso.balde.php modif avec juillet 2013 et 2014

--- resources/views/conso.blade.php
+++ resources/views/conso.blade.php
@@ -6,7 +6,8 @@
 
 <div style='width: 25px;height: 20px; background-color: red; transition: 2s'></div>
 
-<h1>{{ $consototal }}</h1>
+<h1>{{ $consototalJuil13 }}</h1>
+<h1>{{ $consototalJuil14 }}</h1>
 
 @foreach ($consommations as $consommation)
 {{ $consommation->temps }}
