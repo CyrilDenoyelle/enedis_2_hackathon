82e76c95c966b64db663142b5420d5e491c19608
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-06-28 14:33:49 +0200

    finition de l'aiguille du compteur

--- resources/views/gauge.blade.php
+++ resources/views/gauge.blade.php
@@ -3,11 +3,20 @@
 @section('content')
 <link rel='stylesheet' href='/css/gaugestyle.css'>
 <h1>gauge</h1>
-<main style='background-color: blue;'>
+<main>
 <div id='gaugecontainer'>
 	<img src='/img/compteur.png' alt='compteur' id='compteur'>
-	<img src='/img/aiguille.png' alt='aiguille' id='aiguille'>
+	<img src='/img/aiguille.png' alt='aiguille' id='aiguille' style="transition: 2s;">
 	<span id='compteurspan' style=''>{{ $spaneuro }}€/h</span>
 </div>
 </main>
+
+@endsection
+
+@section('scriptation')
+<script>
+	setTimeout(function(){
+		$('#aiguille').css('transform', 'rotate({{ $consommation }}deg')
+	}, 2000)
+</script>
 @endsection
